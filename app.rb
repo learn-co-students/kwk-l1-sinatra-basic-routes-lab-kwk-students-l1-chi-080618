2307871c05c4b216da9d7562ac0a2914725d0d46
dami21 <dami.adenekan@schaumburgchristian.com>
2018-08-14 16:48:47 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/' do
+    "main page"
+  end
+  
+  get '/name' do
+  "My name is Dami!"
+  end
+  get '/hometown' do
+  "My hometown is Schaumburg"
+  end
+  get '/favorite-song' do
+  "My favorite-song is Goat!"
+  end
 end
