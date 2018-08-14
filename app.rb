6e3d602bb5aae9799409e416fd5278aaff12f56b
AnnieGregg <annie.r.gregg@gmail.com>
2018-08-14 19:11:47 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do 
+    "My name is Annie"
+  end
+
+  get '/hometown' do 
+    "My hometown is Orland Hills"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Drive"
+  end
 end
