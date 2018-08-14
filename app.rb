0bfb24cc2532ba71f29b13d2d28fb925afbe027c
NOORABDELHALIM <nur.abdelhalim@rebels.ridgenet.org>
2018-08-14 16:41:57 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+    "My name is Noor"
+  end 
+  get '/hometown' do 
+    "My hometown is Chicago"
+  end 
+  get '/favorite-song' do 
+    "My favorite song is Sweet Creature"
+  end 
 end
